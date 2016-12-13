a05fc8a947f2ccd837099297c41944c4f7bbbcad
Ignacio Gómez <i.gomez10@uniandes.edu.co>
2016-12-13 12:38:31 -0500

    2

--- src/main.js
+++ src/main.js
@@ -2,6 +2,13 @@ import Vue from 'vue'
 import App from './App.vue'
 import Router from 'vue-router'
 import Filev from './fileViewer.vue'
+Vue.use(Router)
+
+const router=new Router(
+  {
+    routes: [{ path: '/:career/:courseCode', component: Filev  }]
+  }
+);
 
 new Vue({
   el: '#app',
